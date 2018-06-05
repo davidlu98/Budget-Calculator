e78b6709f546f3f874a6eadf54e5b6cb84b7123d
David <david124lu@gmail.com>
2018-06-05 09:04:45 -0700

    Minor bug fixed

--- public/js/script.js
+++ public/js/script.js
@@ -60,7 +60,7 @@ $(document).ready(function(){
 	 * Updates the views
 	*/
 	function update_views(type, description, amount, addition_or_removal){
-		amount = amont.toFixed(2);
+		amount = amount.toFixed(2);
 
 		if(displayed_total < 0){
 			var displayed_total_to_string = displayed_total.toString();
