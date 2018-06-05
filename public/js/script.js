c5e2d4b0479738b6c398d8569714291d2fd6d039
David <david124lu@gmail.com>
2018-06-05 08:59:06 -0700

    Changed dates

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 /* script.js
  * The main JavaScript file for the budget calculator
  * Created by David Lu 
- * Date last modified: 6/4/2018 (MM/DD/YYYY)
+ * Date last modified: 6/5/2018 (MM/DD/YYYY)
 */
 
 $(document).ready(function(){
